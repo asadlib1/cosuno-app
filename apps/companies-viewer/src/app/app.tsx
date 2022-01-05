93632644f8a5f70cf7c9538a56605988972ff8e5
Asad Ali Baloch <asad.baloch@10pearls.com>
2022-01-06 01:57:28 +0500

    initial commit

---
+++ apps/companies-viewer/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="companies-viewer" />
+      <div />
+    </>
+  );
+}
+
+export default App;
